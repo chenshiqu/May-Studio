cd5cd3709eea02a83b87dfdcaf603f4a3b15d315
chen_shiqu <348127304@qq.com>
2016-02-02 13:31:29 +0800

    通过传入参数方式加载css文件

--- application/controllers/Maystudio.php
+++ application/controllers/Maystudio.php
@@ -10,21 +10,22 @@ class Maystudio extends CI_Controller
                 $this->load->helper('url');
                 //$this->load->model('studio_model');
         }
-
+        //首页
         public function index()
         {
-                $this->load->view('header');
+                $data['css']=array('index','style');
+                $this->load->view('header',$data);
                 $this->load->view('index');
                 $this->load->view('footer');
         }
-
+        //关于我们
         public function about()
         {
                 $this->load->view('header');
                 $this->load->view('about');
                 $this->load->view('footer');
         }
-
+        //联系我们
         public function contact_info()
         {
                 $this->load->view('header');
