1eb6039aaed138043d8b83eb74f8d5e8d97e3e2d
chen_shiqu <348127304@qq.com>
2016-03-06 10:45:26 +0800

    修改config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/May-Studio/';
+$config['base_url'] = 'http://1.mayworkspace.applinzi.com/';
 
 /*
 |--------------------------------------------------------------------------
