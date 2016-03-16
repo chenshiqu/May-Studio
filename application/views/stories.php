bf5183c666f486d16fe3fdce0f2cd7a9640201d4
chen_shiqu <348127304@qq.com>
2016-03-16 15:47:58 +0800

    数据库读取显示

--- application/views/stories.php
+++ application/views/stories.php
@@ -1,32 +1,27 @@
     <section id="content">
     	<div id="container">
     		<div class="content-left">
+        		              <ul>
+                                                        <?php for($i=0;$i<5;$i++){ ?>
+                                                        <li>
+                                                                <a href=" <?php echo base_url(); ?>index.php/maystudio/episode/<?php echo $result[$i]['id'] ?>">
+                                                                        <?php echo "EP".$result[$i]['picture'].".".$result[$i]['title']  ?>
+                                                                </a>
+                                                        </li>
+                                                        <?php } ?>
+            	                            </ul>
+        	              </div>
+        	              <div class="content-right">
         		<ul>
-            		<li><a href="<?php echo base_url(); ?>index.php/maystudio/episode">EP20. 一碗鸡汤</a></li>
-                	<li><a href="<?php echo base_url(); ?>index.php/maystudio/episode">EP19. 二零一六年的第一天，怎么办</a></li>
-                	<li><a href="<?php echo base_url(); ?>index.php/maystudio/episode">EP18. 二零一五年的最后一天，怎么办</a></li>
-                	<li><a href="<?php echo base_url(); ?>index.php/maystudio/episode">EP17. 灰蓝旅行社之牧年少羊的奇幻之旅</a></li>
-                	<li><a href="<?php echo base_url(); ?>index.php/maystudio/episode">EP16. 武林盟主速成手册</a></li>
-                	<li><a href="<?php echo base_url(); ?>index.php/maystudio/episode">EP15. 论坐大巴时的优越感</a></li>
-                	<li><a href="<?php echo base_url(); ?>index.php/maystudio/episode">EP14. 灰蓝旅行社</a></li>
-                	<li><a href="<?php echo base_url(); ?>index.php/maystudio/episode">EP13. 生活小贴士之如何穿睡衣出门</a></li>
-                	<li><a href="<?php echo base_url(); ?>index.php/maystudio/episode">EP12. 动物园的那些事</a></li>
-                	<li><a href="<?php echo base_url(); ?>index.php/maystudio/episode">EP11. 2035年流行趋势剖析</a></li>
-                	<li><a href="<?php echo base_url(); ?>index.php/maystudio/episode">EP10. 动物园里的百兽之王</a></li>
-                	<li><a href="<?php echo base_url(); ?>index.php/maystudio/episode">EP09. 生活小贴士之脱毛篇</a></li>
-                	<li><a href="<?php echo base_url(); ?>index.php/maystudio/episode">EP08. 续一个故事</a></li>
-                	<li><a href="<?php echo base_url(); ?>index.php/maystudio/episode">EP07. 灰蓝牌洗衣机</a></li>
-                	<li><a href="<?php echo base_url(); ?>index.php/maystudio/episode">EP06. 讲一个故事</a></li>
-            	</ul>
-        	</div>
-        	<div class="content-right">
-        		<ul>
-                	<li><a href="<?php echo base_url(); ?>index.php/maystudio/episode">EP05. 人类进化新方向</a></li>
-                	<li><a href="<?php echo base_url(); ?>index.php/maystudio/episode">EP04. 信用卡的额外功能</a></li>
-                	<li><a href="<?php echo base_url(); ?>index.php/maystudio/episode">EP03. 独立自强新女性必备小工具</a></li>
-                	<li><a href="<?php echo base_url(); ?>index.php/maystudio/episode">EP02. 实习感想</a></li>
-                	<li><a href="<?php echo base_url(); ?>index.php/maystudio/episode">EP01. 生活小贴士之剃须篇</a></li>
-            	</ul>
-        	</div>
+                	               <?php for($i=5;$i<10;$i++){ ?>
+                                                        <li>
+                                                                    <a href="<?php echo base_url(); ?>index.php/maystudio/episode/<?php echo $result[$i]['id'] ?>">
+                                                                                <?php echo "EP".$result[$i]['picture'].".".$result[$i]['title']  ?>
+                                                                    </a>
+                                                        </li>
+                                          <?php } ?>
+            	              </ul>
+        	              </div>
+                          <div><?php echo $this->pagination->create_links(); ?></div>
     	</div>
     </section>
