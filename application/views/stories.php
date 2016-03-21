36b5a9e1439451efd5aecd3882c8d3eb548e74ac
chen_shiqu <348127304@qq.com>
2016-03-21 16:03:03 +0800

    修改故事显示bug

--- application/views/stories.php
+++ application/views/stories.php
@@ -2,23 +2,27 @@
     	<div id="container">
     		<div class="content-left">
         		              <ul>
-                                                        <?php for($i=0;$i<5;$i++){ ?>
-                                                        <li>
-                                                                <a href=" <?php echo base_url(); ?>index.php/maystudio/episode/<?php echo $result[$i]['id'] ?>">
-                                                                        <?php echo "EP".$result[$i]['picture'].".".$result[$i]['title']  ?>
-                                                                </a>
-                                                        </li>
+                                                        <?php for($i=0;$i<5;$i++){ 
+                                                                    if(isset($result[$i])) { ?>
+                                                                                    <li>
+                                                                                                <a href=" <?php echo base_url(); ?>index.php/maystudio/episode/<?php echo $result[$i]['id'] ?>">
+                                                                                                    <?php echo "EP".$result[$i]['picture'].".".$result[$i]['title']  ?>
+                                                                                                </a>
+                                                                                    </li>
+                                                                      <?php } ?>
                                                         <?php } ?>
             	                            </ul>
         	              </div>
         	              <div class="content-right">
         		<ul>
-                	               <?php for($i=5;$i<10;$i++){ ?>
-                                                        <li>
-                                                                    <a href="<?php echo base_url(); ?>index.php/maystudio/episode/<?php echo $result[$i]['id'] ?>">
-                                                                                <?php echo "EP".$result[$i]['picture'].".".$result[$i]['title']  ?>
-                                                                    </a>
-                                                        </li>
+                	               <?php for($i=5;$i<10;$i++){
+                                                        if(isset($result[$i])) { ?>
+                                                                    <li>
+                                                                                    <a href=" <?php echo base_url(); ?>index.php/maystudio/episode/<?php echo $result[$i]['id'] ?>">
+                                                                                                <?php echo "EP".$result[$i]['picture'].".".$result[$i]['title']  ?>
+                                                                                    </a>
+                                                                    </li>
+                                                        <?php } ?>
                                           <?php } ?>
             	              </ul>
         	              </div>
