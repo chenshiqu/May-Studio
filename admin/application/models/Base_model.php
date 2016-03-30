6677ea19f38b82dc1800f6f7b7aa9ec49e9edf55
chen_shiqu <348127304@qq.com>
2016-03-30 23:33:45 +0800

    完善basemodel

--- admin/application/models/Base_model.php
+++ admin/application/models/Base_model.php
@@ -40,4 +40,34 @@ class Base_model extends CI_Model
 		$row_num=$this->db->count_all($table);
 		return $row_num;
 	}
+
+	/**
+	 * delete a recode from table by id 
+	 * @param $table string 
+	 * 	$id int
+	 * @return bool 
+	 */
+	public function deleteOneById($table,$story_id)
+	{
+		$sql="delete from $table where id=$story_id";
+		$result=$this->db->query($sql);
+		return $result;
+	}
+
+	/**
+	 * get story data by setting offset and limit
+	 * @param $table string 
+	 * 	      $limit  number     the number of record will return 
+	 * 	      $offset number    
+	 * @return array 
+	 *      		array([0]=>{'id'=>,'title'=>},[1]=>{})
+	 */
+	public function get_num_rows($table,$limit,$offset)
+	{
+		if($offset=="")
+			$offset=0;
+		$sql="select * from (select * from $table order by public_time desc)a limit {$offset},{$limit}";
+		$query=$this->db->query($sql);
+		return $query->result_array();
+	}
 }
